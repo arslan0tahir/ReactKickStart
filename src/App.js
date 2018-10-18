e8600fd84e2dc114fa605b5f0053f5a16d285f7c
Arslan Tahir <arslan.tahir@outlook.com>
2018-10-18 05:57:00 +0500

    Radium inline sudo selector styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'  // all components names should be in uppercase
-
+import Radium from 'radium'
 
 class App extends Component {
 
@@ -63,7 +63,12 @@ class App extends Component {
       font: 'inherit',
       border : '1px solid blue',
       padding : '8px',
-      cursor : 'pointer'
+      cursor : 'pointer',
+      ':hover' : {
+        backgroundColor: 'lightgreen',
+        color:'black'
+
+      }
     }
 
     let persons=null;
@@ -85,6 +90,9 @@ class App extends Component {
         </div>
       )
       style.backgroundColor="red";
+      style[':hover'].color="black";
+      style[':hover'].backgroundColor="salmon";
+
     }
 
     const classes=[];
@@ -111,4 +119,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
